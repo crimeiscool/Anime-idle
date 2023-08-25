4841b8e2c44b2e8730b1349804c89ffe5124ffb6
crimeiscool <139124034+crimeiscool@users.noreply.github.com>
2023-08-25 21:51:34 +0200

    Update animeidle.lua

--- animeidle.lua
+++ animeidle.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/crimeiscool/Anime-idle/blob/main/animeidle.lua"))()
 local Window = Library.CreateLib("Anime Idle Simulator Script - Sakai#7392", "GrapeTheme")
 
 local Tab = Window:NewTab("AutoFarm")
